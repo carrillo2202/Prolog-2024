2adb3146a9e29b438eab8eb687190a3f61f88b59
Brayan Carrillo <carrilloolmedobrayan@gmail.com>
2024-10-01 18:17:40 -0600

    Avance parentesis

--- ParentesisPila/Parentesis.java
+++ ParentesisPila/Parentesis.java
@@ -1,5 +1,10 @@
 package ParentesisPila;
 
 public class Parentesis {
-    
+
+    public static void main(String[] args) {
+        // Expresiones de prueba
+        String expression = "(()())"; // Balanceada
+        
+    }
 }
