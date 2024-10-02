79cc7c95c4e5932c37bb1c99c8565ae8788fd51f
Brayan Carrillo <carrilloolmedobrayan@gmail.com>
2024-10-01 18:23:12 -0600

    Avance parentesis 2

--- ParentesisPila/Parentesis.java
+++ ParentesisPila/Parentesis.java
@@ -1,10 +1,26 @@
 package ParentesisPila;
 
+import java.util.Stack;
 public class Parentesis {
 
+     // Método para verificar si los paréntesis están balanceados
+     public static boolean isBalanced(String expression) {
+        // Crear una pila para almacenar los paréntesis
+        Stack<Character> stack = new Stack<>();
+
+      
+        return stack.isEmpty();
+    }
+    
     public static void main(String[] args) {
         // Expresiones de prueba
-        String expression = "(()())"; // Balanceada
-        
+        String expression1 = "(()())"; 
+        String expression2 = "((())"; 
+        String expression3 = "())(";   
+
+        // Evaluar si las expresiones están balanceadas
+        System.out.println("Expression 1: " + isBalanced(expression1)); 
+        System.out.println("Expression 2: " + isBalanced(expression2)); 
+        System.out.println("Expression 3: " + isBalanced(expression3));
     }
 }
