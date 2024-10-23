0f6df4f0651c8bdb67ce5d2539dccbb1d4108fd2
Brayan Carrillo <carrilloolmedobrayan@gmail.com>
2024-10-23 09:18:50 -0600

    Modificacion base de conocimientos

--- Prolog/familia.pl
+++ Prolog/familia.pl
@@ -1,5 +1,6 @@
 % Hechos
 hombre(francisco).
+hombre(francisco_jr).
 hombre(brayan).
 hombre(gumercindo).
 mujer(paola).
@@ -10,18 +11,13 @@ padre(francisco, brayan).
 madre(paola, francisco_jr).
 madre(paola, brayan).
 
-% abuelos
-padre(gumercindo, paola).
-madre(blanca, paola).
+padrede(gumercindo, paola).
+madrede(blanca, paola).
 
 % Regla para abuelos
-abuelo(X, Y) :- padre(X, Z), padres(Z, Y).
-abuela(X, Y) :- madre(X, Z), padres(Z, Y).
+abuelo(X, Y) :- padrede(X, Z), padres(Z, Y).
+abuela(X, Y) :- madrede(X, Z), padres(Z, Y).
 
 % Regla para padres 
 padres(X, Y) :- padre(X, Y); madre(X, Y).
 
-% Consultas de ejemplo:
-% - hombre(francisco).
-% - padre(francisco, brayan).
-% - abuelo(francisco, francisco_jr).
