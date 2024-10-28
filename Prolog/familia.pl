36675bf9b16b8d53ee000d30e772e7f7d29b8d46
Brayan Carrillo <carrilloolmedobrayan@gmail.com>
2024-10-27 22:27:55 -0600

    Reglas completas de familia prolog

--- Prolog/familia.pl
+++ Prolog/familia.pl
@@ -3,11 +3,13 @@ hombre(francisco).
 hombre(francisco_jr).
 hombre(brayan).
 hombre(gumercindo).
+hombre(edy).
 mujer(paola).
 mujer(blanca).
 mujer(karina).
 mujer(valeria).
 mujer(yocelyn).
+
 
 padre(francisco, francisco_jr).
 padre(francisco, brayan).
@@ -21,6 +23,8 @@ madre(karina, valeria).
 madre(karina, yocelyn).
 madre(blanca, paola).
 madre(blanca, karina).
+esposos(francisco, paola).
+esposos(edy, karina).
 
 
 % Regla para abuelos
@@ -35,3 +39,31 @@ hermanos(X, Y) :- (padre(Z, X), padre(Z, Y); madre(W, X), madre(W, Y)), X \= Y.
 
 % Regla para primos
 primos(X, Y) :- padres(A, X), padres(B, Y), hermanos(A, B).
+
+% Relación de cuñados
+cunado(X, Y) :- esposos(X, Z), hermanos(Z, Y). 
+cunado(X, Y) :- esposos(Y, Z), hermanos(Z, X). 
+
+% Relación de tíos y tías (incluyendo cónyuges)
+tio(X, Y) :- hermanos(X, Z), padres(Z, Y), hombre(X).          
+tio(X, Y) :- esposos(X, W), tia(W, Y).                         
+tia(X, Y) :- hermanos(X, Z), padres(Z, Y), mujer(X).           
+tia(X, Y) :- esposos(X, W), tio(W, Y).                         
+
+% sobrinos
+sobrino(X, Y) :- tio(Y, X), hombre(X).
+sobrino(X, Y) :- tia(Y, X), hombre(X).
+sobrina(X, Y) :- tio(Y, X), mujer(X).
+sobrina(X, Y) :- tia(Y, X), mujer(X).
+
+% Nietos
+nieto(X, Y) :- abuelo(Y, X), hombre(X).
+nieto(X, Y) :- abuela(Y, X), hombre(X).
+nieta(X, Y) :- abuelo(Y, X), mujer(X).
+nieta(X, Y) :- abuela(Y, X), mujer(X).
+
+% Hijos
+hijo(X, Y) :- padre(Y, X), hombre(X).
+hijo(X, Y) :- madre(Y, X), hombre(X).
+hija(X, Y) :- padre(Y, X), mujer(X).
+hija(X, Y) :- madre(Y, X), mujer(X).
